72afd7b528125907b8f6663057dbb7ba72459e6b
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-23 12:09:49 -0600

    Update index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,13 +3,10 @@ import cors from 'cors';
 import apiRoutes from './routes/api';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
