241239a30d846151b29165148899fa473f2305b6
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-23 10:55:42 -0600

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Roadmap from './components/Roadmap';
+
+function App() {
+  return (
+    <div>
+      <h1>Cybersecurity Roadmap Creation Tool</h1>
+      <Roadmap />
+    </div>
+  );
+}
+
+export default App;
