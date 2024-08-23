b54355c312bb142fdc631f6be7b57812254b4320
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-23 17:17:46 -0600

    Update RoadmapGanttChart.tsx

--- src/components/RoadmapGanttChart.tsx
+++ src/components/RoadmapGanttChart.tsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { RootState } from '../redux/store';
 import { Typography, Box } from '@mui/material';
+import { Task, VCISOTask } from '../types';
 
 const RoadmapGanttChart: React.FC = () => {
   const { frameworks, selectedFramework, vcisoTasks } = useSelector((state: RootState) => state.roadmap);
@@ -13,12 +14,23 @@ const RoadmapGanttChart: React.FC = () => {
 
   const tasks = selectedFramework ? selectedTasks : vcisoTasks;
 
-  const data = tasks.map(task => ({
-    name: 'Task' in task ? task.Task : task.name,
-    start: 'Timeline - Start' in task ? new Date(task['Timeline - Start']).getTime() : task.start.getTime(),
-    end: 'Timeline - End' in task ? new Date(task['Timeline - End']).getTime() : task.end.getTime(),
-    status: 'Status' in task ? task.Status : task.status,
-  }));
+  const data = tasks.map(task => {
+    const isVCISOTask = 'Task' in task;
+    return {
+      name: isVCISOTask ? (task as VCISOTask).Task : (task as Task).name,
+      start: isVCISOTask 
+        ? (task as VCISOTask)['Timeline - Start'] 
+          ? new Date((task as VCISOTask)['Timeline - Start']).getTime() 
+          : undefined
+        : (task as Task).start.getTime(),
+      end: isVCISOTask
+        ? (task as VCISOTask)['Timeline - End']
+          ? new Date((task as VCISOTask)['Timeline - End']).getTime()
+          : undefined
+        : (task as Task).end.getTime(),
+      status: isVCISOTask ? (task as VCISOTask).Status : (task as Task).status,
+    };
+  });
 
   if (data.length === 0) {
     return <Typography>Please select a framework or generate a vCISO roadmap to view the chart.</Typography>;
@@ -32,11 +44,16 @@ const RoadmapGanttChart: React.FC = () => {
           data={data}
           margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
         >
-          <XAxis type="number" dataKey="start" domain={['dataMin', 'dataMax']} tickFormatter={(unixTime) => new Date(unixTime).toLocaleDateString()} />
+          <XAxis 
+            type="number" 
+            dataKey="start" 
+            domain={['dataMin', 'dataMax']} 
+            tickFormatter={(unixTime) => unixTime ? new Date(unixTime).toLocaleDateString() : ''}
+          />
           <YAxis type="category" dataKey="name" width={150} />
           <Tooltip
-            labelFormatter={(value) => new Date(value).toLocaleDateString()}
-            formatter={(value: any) => [new Date(value).toLocaleDateString(), 'Date']}
+            labelFormatter={(value) => value ? new Date(value).toLocaleDateString() : ''}
+            formatter={(value: any) => [value ? new Date(value).toLocaleDateString() : '', 'Date']}
           />
           <Legend />
           <Bar dataKey="start" stackId="a" fill="#8884d8" />
