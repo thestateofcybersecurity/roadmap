1079163b3c4252558bf72274798afb66f0b97252
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-23 11:18:12 -0600

    Update and rename api-utils.ts to client/src/utils/api.ts

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Framework } from '../types';
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'https://thestateofcybersecurity.github.io/roadmap/api';
 
 export const fetchFrameworks = async (): Promise<Framework[]> => {
   const response = await axios.get(`${API_BASE_URL}/frameworks`);
