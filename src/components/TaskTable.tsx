1becb24589dfe2f0bdabd5b9b3e3e323cb343ecf
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-23 16:41:30 -0600

    Update TaskTable.tsx

--- src/components/TaskTable.tsx
+++ src/components/TaskTable.tsx
@@ -3,22 +3,27 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField, Select, MenuItem } from '@mui/material';
 import { RootState } from '../redux/store';
 import { updateTask } from '../redux/roadmapSlice';
-import { Task } from '../types';
+import { Task, VCISOTask } from '../types';
 
 const TaskTable: React.FC = () => {
   const dispatch = useDispatch();
-  const { frameworks, selectedFramework } = useSelector((state: RootState) => state.roadmap);
+  const { frameworks, selectedFramework, vcisoTasks } = useSelector((state: RootState) => state.roadmap);
 
   const selectedTasks = selectedFramework
     ? frameworks.find(f => f.id === selectedFramework)?.tasks || []
     : [];
 
-  const handleTaskChange = (taskId: string, field: keyof Task, value: any) => {
-    const task = selectedTasks.find(t => t.id === taskId);
-    if (task && selectedFramework) {
-      const updatedTask = { ...task, [field]: value };
-      dispatch(updateTask({ frameworkId: selectedFramework, task: updatedTask }));
+  const tasks = selectedFramework ? selectedTasks : vcisoTasks;
+
+  const handleTaskChange = (taskId: string, field: keyof (Task | VCISOTask), value: any) => {
+    if (selectedFramework) {
+      const task = selectedTasks.find(t => t.id === taskId) as Task;
+      if (task) {
+        const updatedTask = { ...task, [field]: value };
+        dispatch(updateTask({ frameworkId: selectedFramework, task: updatedTask }));
+      }
     }
+    // For vCISO tasks, we might want to implement a separate update action
   };
 
   return (
@@ -30,42 +35,50 @@ const TaskTable: React.FC = () => {
             <TableCell>Start Date</TableCell>
             <TableCell>End Date</TableCell>
             <TableCell>Status</TableCell>
+            {!selectedFramework && <TableCell>Package</TableCell>}
+            {!selectedFramework && <TableCell>Estimated Hours</TableCell>}
           </TableRow>
         </TableHead>
         <TableBody>
-          {selectedTasks.map((task) => (
-            <TableRow key={task.id}>
+          {tasks.map((task) => (
+            <TableRow key={'id' in task ? task.id : task.Task}>
               <TableCell>
                 <TextField
                   fullWidth
-                  value={task.name}
-                  onChange={(e) => handleTaskChange(task.id, 'name', e.target.value)}
+                  value={'Task' in task ? task.Task : task.name}
+                  onChange={(e) => handleTaskChange('id' in task ? task.id : task.Task, 'Task' in task ? 'Task' : 'name', e.target.value)}
                 />
               </TableCell>
               <TableCell>
                 <TextField
                   type="date"
-                  value={task.start.toISOString().split('T')[0]}
-                  onChange={(e) => handleTaskChange(task.id, 'start', new Date(e.target.value))}
+                  value={'Timeline - Start' in task ? task['Timeline - Start'] : task.start.toISOString().split('T')[0]}
+                  onChange={(e) => handleTaskChange('id' in task ? task.id : task.Task, 'Timeline - Start' in task ? 'Timeline - Start' : 'start', new Date(e.target.value))}
                 />
               </TableCell>
               <TableCell>
                 <TextField
                   type="date"
-                  value={task.end.toISOString().split('T')[0]}
-                  onChange={(e) => handleTaskChange(task.id, 'end', new Date(e.target.value))}
+                  value={'Timeline - End' in task ? task['Timeline - End'] : task.end.toISOString().split('T')[0]}
+                  onChange={(e) => handleTaskChange('id' in task ? task.id : task.Task, 'Timeline - End' in task ? 'Timeline - End' : 'end', new Date(e.target.value))}
                 />
               </TableCell>
               <TableCell>
                 <Select
-                  value={task.status}
-                  onChange={(e) => handleTaskChange(task.id, 'status', e.target.value)}
+                  value={'Status' in task ? task.Status : task.status}
+                  onChange={(e) => handleTaskChange('id' in task ? task.id : task.Task, 'Status' in task ? 'Status' : 'status', e.target.value)}
                 >
                   <MenuItem value="New">New</MenuItem>
                   <MenuItem value="In Progress">In Progress</MenuItem>
                   <MenuItem value="Completed">Completed</MenuItem>
                 </Select>
               </TableCell>
+              {!selectedFramework && (
+                <>
+                  <TableCell>{(task as VCISOTask).Package}</TableCell>
+                  <TableCell>{(task as VCISOTask)['Estimated vCISO HR(s)']}</TableCell>
+                </>
+              )}
             </TableRow>
           ))}
         </TableBody>
