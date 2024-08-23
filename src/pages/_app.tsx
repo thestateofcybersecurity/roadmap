9da47b2b958ad8da224311f36529979119bd9701
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-23 12:12:19 -0600

    Create _app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import Layout from '../components/Layout';
+import { store } from '../redux/store';
+import '../styles/globals.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1e3a8a',
+    },
+    secondary: {
+      main: '#10b981',
+    },
+    background: {
+      default: '#f3f4f6',
+    },
+  },
+  typography: {
+    fontFamily: 'Inter, sans-serif',
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+      },
+    },
+  },
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </Provider>
+  );
+}
+
+export default MyApp;
