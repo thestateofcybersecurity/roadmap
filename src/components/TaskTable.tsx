0b7afb13a2a32d592de980db846ca4941b9647a8
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-24 12:00:46 -0600

    Update TaskTable.tsx

--- src/components/TaskTable.tsx
+++ src/components/TaskTable.tsx
@@ -1,96 +1,45 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField, Select, MenuItem } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from '@mui/material';
 import { RootState } from '../redux/store';
-import { updateTask, updateVCISOTask } from '../redux/roadmapSlice';
-import { Task, VCISOTask } from '../types';
+import { CISControl } from '../types';
 
 const TaskTable: React.FC = () => {
-  const dispatch = useDispatch();
-  const { frameworks, selectedFramework, vcisoTasks } = useSelector((state: RootState) => state.roadmap);
+  const { selectedFramework, frameworkData, filters } = useSelector((state: RootState) => state.roadmap);
 
-  const selectedTasks = selectedFramework
-    ? frameworks.find(f => f.id === selectedFramework)?.tasks || []
-    : [];
+  const filteredData = frameworkData.filter((item: CISControl) => 
+    filters.riskLevels.includes(item.RISK) &&
+    filters.implementationGroups.some(ig => item[ig as keyof CISControl] !== NaN && item[ig as keyof CISControl] !== '')
+  );
 
-  const tasks = selectedFramework ? selectedTasks : vcisoTasks;
-
-  const handleTaskChange = (task: Task | VCISOTask, field: string, value: any) => {
-    if ('id' in task) {
-      // This is a regular Task
-      const updatedTask = { ...task, [field]: value };
-      dispatch(updateTask({ frameworkId: selectedFramework!, task: updatedTask }));
-    } else {
-      // This is a VCISOTask
-      const updatedTask = { ...task, [field]: value };
-      dispatch(updateVCISOTask(updatedTask));
-    }
-  };
-
-  const formatDate = (date: Date | string | undefined): string => {
-    if (!date) return '';
-    if (typeof date === 'string') return date.split('T')[0];
-    return date.toISOString().split('T')[0];
-  };
-
-  const isVCISOTask = (task: Task | VCISOTask): task is VCISOTask => {
-    return 'Task' in task;
-  };
+  if (!selectedFramework || filteredData.length === 0) {
+    return <Typography>No data to display. Please select a framework and apply filters.</Typography>;
+  }
 
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>Task Name</TableCell>
-            <TableCell>Start Date</TableCell>
-            <TableCell>End Date</TableCell>
-            <TableCell>Status</TableCell>
-            {!selectedFramework && <TableCell>Package</TableCell>}
-            {!selectedFramework && <TableCell>Estimated Hours</TableCell>}
+            <TableCell>Title</TableCell>
+            <TableCell>Risk</TableCell>
+            <TableCell>Implementation Groups</TableCell>
+            <TableCell>CIS Control</TableCell>
+            <TableCell>CIS Sub-Control</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {tasks.map((task) => (
-            <TableRow key={isVCISOTask(task) ? task.Task : task.id}>
+          {filteredData.map((item: CISControl, index) => (
+            <TableRow key={index}>
+              <TableCell>{item.Title}</TableCell>
+              <TableCell>{item.RISK}</TableCell>
               <TableCell>
-                <TextField
-                  fullWidth
-                  value={isVCISOTask(task) ? task.Task : task.name}
-                  onChange={(e) => handleTaskChange(task, isVCISOTask(task) ? 'Task' : 'name', e.target.value)}
-                />
+                {['IG1', 'IG2', 'IG3']
+                  .filter(ig => item[ig as keyof CISControl] !== NaN && item[ig as keyof CISControl] !== '')
+                  .join(', ')}
               </TableCell>
-              <TableCell>
-                <TextField
-                  type="date"
-                  value={formatDate(isVCISOTask(task) ? task['Timeline - Start'] : task.start)}
-                  onChange={(e) => handleTaskChange(task, isVCISOTask(task) ? 'Timeline - Start' : 'start', e.target.value)}
-                />
-              </TableCell>
-              <TableCell>
-                <TextField
-                  type="date"
-                  value={formatDate(isVCISOTask(task) ? task['Timeline - End'] : task.end)}
-                  onChange={(e) => handleTaskChange(task, isVCISOTask(task) ? 'Timeline - End' : 'end', e.target.value)}
-                />
-              </TableCell>
-              <TableCell>
-                <Select
-                  value={isVCISOTask(task) ? (task.Status || '') : task.status}
-                  onChange={(e) => handleTaskChange(task, isVCISOTask(task) ? 'Status' : 'status', e.target.value)}
-                >
-                  <MenuItem value="">Not Set</MenuItem>
-                  <MenuItem value="New">New</MenuItem>
-                  <MenuItem value="In Progress">In Progress</MenuItem>
-                  <MenuItem value="Completed">Completed</MenuItem>
-                </Select>
-              </TableCell>
-              {!selectedFramework && isVCISOTask(task) && (
-                <>
-                  <TableCell>{task.Package}</TableCell>
-                  <TableCell>{task['Estimated vCISO HR(s)']}</TableCell>
-                </>
-              )}
+              <TableCell>{item['CIS Control']}</TableCell>
+              <TableCell>{item['CIS Sub-Control']}</TableCell>
             </TableRow>
           ))}
         </TableBody>
