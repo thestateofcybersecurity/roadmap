cb50efe1c7ea08dea90895ccf071e7dfc11ad0f7
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-24 11:57:53 -0600

    Update roadmapSlice.ts

--- src/redux/roadmapSlice.ts
+++ src/redux/roadmapSlice.ts
@@ -1,43 +1,34 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RoadmapState, Framework, Task, VCISOTask } from '../types';
+import { RoadmapState, Framework, Task } from '../types';
 
 const initialState: RoadmapState = {
-  frameworks: [],
+  frameworks: ['CIS'], // Add more frameworks as needed
   selectedFramework: null,
-  vcisoTasks: [],
+  frameworkData: [],
+  filters: {
+    riskLevels: [],
+    implementationGroups: []
+  }
 };
 
 const roadmapSlice = createSlice({
   name: 'roadmap',
   initialState,
   reducers: {
-    setFrameworks: (state, action: PayloadAction<Framework[]>) => {
+    setFrameworks: (state, action: PayloadAction<string[]>) => {
       state.frameworks = action.payload;
     },
     selectFramework: (state, action: PayloadAction<string>) => {
       state.selectedFramework = action.payload;
     },
-    updateTask: (state, action: PayloadAction<{ frameworkId: string; task: Task }>) => {
-      const { frameworkId, task } = action.payload;
-      const framework = state.frameworks.find(f => f.id === frameworkId);
-      if (framework) {
-        const taskIndex = framework.tasks.findIndex(t => t.id === task.id);
-        if (taskIndex !== -1) {
-          framework.tasks[taskIndex] = task;
-        }
-      }
+    setFrameworkData: (state, action: PayloadAction<any[]>) => {
+      state.frameworkData = action.payload;
     },
-    setVCISOTasks: (state, action: PayloadAction<VCISOTask[]>) => {
-      state.vcisoTasks = action.payload;
-    },
-    updateVCISOTask: (state, action: PayloadAction<VCISOTask>) => {
-      const taskIndex = state.vcisoTasks.findIndex(t => t.Task === action.payload.Task);
-      if (taskIndex !== -1) {
-        state.vcisoTasks[taskIndex] = action.payload;
-      }
+    setFilters: (state, action: PayloadAction<{ riskLevels: string[], implementationGroups: string[] }>) => {
+      state.filters = action.payload;
     },
   },
 });
 
-export const { setFrameworks, selectFramework, updateTask, setVCISOTasks, updateVCISOTask } = roadmapSlice.actions;
+export const { setFrameworks, selectFramework, setFrameworkData, setFilters } = roadmapSlice.actions;
 export default roadmapSlice.reducer;
